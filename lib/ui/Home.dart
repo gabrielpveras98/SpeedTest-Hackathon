02424b46845e68b905b044f6fdc179e54cf3dfd5
gabrielpveras98 <gabrielpveras98@gmail.com>
2019-11-08 08:36:56 -0300

    app 04

--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -4,67 +4,139 @@ import 'package:menino_da_ti/ui/childs/Formatacao.dart';
 import 'package:menino_da_ti/ui/childs/childs/PrestadoresDeServico.dart';
 
 class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.red, title: Text("Menino da TI", style: TextStyle(fontStyle: FontStyle.italic, color: Colors.white, ),textAlign: TextAlign.center,), centerTitle: true,),
-      body: SingleChildScrollView(child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child:
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                tecla("Formatação", Icons.computer, context, PrestadoresDeServico()),
-                tecla("Suporte técnico", Icons.person, context, Formatacao()),
-              ],),
-          ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child:
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                tecla("Reparos genéricos", Icons.build, context, Formatacao()),
-                tecla("Wi-fi", Icons.wifi, context, Formatacao()),
-              ],
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(
+            "Menino da TI",
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: Colors.white,
             ),
+            textAlign: TextAlign.center,
           ),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child:
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                tecla("SmartTV", Icons.tv, context, Formatacao()),
-                tecla("Peças e acessórios", Icons.shopping_cart, context, Formatacao()),
-              ],
-            ),
+          centerTitle: true,
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: tab,
           ),
-        ],
-      ),)
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: adicionar(context).map((Widget tab) {
+            return Center(child: tab);
+          }).toList(),
+        ),
+      ),
     );
   }
+
+  List<Widget> adicionar(BuildContext context) {
+    List<Widget> myTabs = <Widget>[homeTab(context), Text("teste"), Text("teste")];
+    return myTabs;
+  }
+
+  List<Tab> tab = [
+    Tab(text: 'Home'),
+    Tab(text: 'Acompar'),
+    Tab(text: 'Teste'),
+  ];
+
+  TabController _tabController;
 }
 
 Widget tecla(String text, IconData icons, BuildContext context, Widget widget) {
   return GestureDetector(
     child: Column(
       children: <Widget>[
-        Padding(child: Icon(icons, size: 40, color: Colors.lightBlueAccent,), padding: EdgeInsets.all(15.0),),
-        Padding(child: Text(text, textAlign: TextAlign.center, style: TextStyle(fontSize: 20, color: Colors.blueGrey),), padding: EdgeInsets.all(15.0),)
+        Padding(
+          child: Icon(
+            icons,
+            size: 40,
+            color: Colors.lightBlueAccent,
+          ),
+          padding: EdgeInsets.all(15.0),
+        ),
+        Padding(
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, color: Colors.blueGrey),
+          ),
+          padding: EdgeInsets.all(15.0),
+        )
       ],
     ),
-    onTap: (){
+    onTap: () {
       Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
     },
   );
+}
+
+Widget homeTab(BuildContext context) {
+  return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  tecla("Formatação", Icons.computer, context,
+                      PrestadoresDeServico()),
+                  tecla("Suporte técnico", Icons.person, context, Formatacao()),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  tecla(
+                      "Reparos genéricos", Icons.build, context, Formatacao()),
+                  tecla("Wi-fi", Icons.wifi, context, Formatacao()),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  tecla("SmartTV", Icons.tv, context, Formatacao()),
+                  tecla("Peças e acessórios", Icons.shopping_cart, context,
+                      Formatacao()),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ));
 }
